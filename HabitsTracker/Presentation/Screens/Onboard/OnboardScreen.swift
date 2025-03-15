520a36a9af56e10c5ea16235f4191574c29da790
KsArT-IT <ksart.it@gmail.com>
2025-03-15 18:01:04 +0200

    feat: add OnboardScreen component for onboarding experience

---
+++ HabitsTracker/Presentation/Screens/Onboard/OnboardScreen.swift
@@ -0,0 +1,40 @@
+//
+//  OnboardScreen.swift
+//  HabitsTracker
+//
+//  Created by KsArT on 14.03.2025.
+//
+
+import SwiftUI
+
+struct OnboardScreen: View {
+    var body: some View {
+        GeometryReader { geometry in
+            let size = max(geometry.size.width, geometry.size.height)
+            
+            VStack {
+                VStack {
+                    Image(.onboardLogo)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                VStack {
+                    Image(.hello)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                    PrimaryButton(label: "Continue") {
+                        
+                    }
+                    .padding(.bottom)
+                }
+            }
+            .padding(.horizontal)
+            .radialGradientBackground(
+                startRadius: size * Constants.Sizes.radialGradientStartPercent,
+                endRadius: size * Constants.Sizes.radialGradientEndPercent
+            )
+        }
+    }
+}
+
+#Preview {
+    OnboardScreen()
+}
