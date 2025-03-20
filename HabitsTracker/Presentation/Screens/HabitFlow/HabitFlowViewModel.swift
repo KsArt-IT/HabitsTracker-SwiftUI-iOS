fca10c665f216087c75a784ad024e7ade0c6a086
KsArT-IT <ksart.it@gmail.com>
2025-03-20 18:33:16 +0200

    feat: add HabitFlowViewModel for managing habit flow logic

---
+++ HabitsTracker/Presentation/Screens/HabitFlow/HabitFlowViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HabitFlowViewModel.swift
+//  HabitCurrent-SUI
+//
+//  Created by KsArT on 11.03.2025.
+//
+
+import Foundation
+
+final class HabitFlowViewModel: ObservableObject {
+    
+}
