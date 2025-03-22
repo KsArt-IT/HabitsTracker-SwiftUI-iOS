f7ca55c14c30dfcd64b79cb552fe17636b365437
KsArT-IT <ksart.it@gmail.com>
2025-03-22 18:53:39 +0200

    feat: replace TabView with SegmentedView for improved tab selection in MainScreen

--- HabitsTracker/Presentation/Screens/Main/MainScreen.swift
+++ HabitsTracker/Presentation/Screens/Main/MainScreen.swift
@@ -9,64 +9,44 @@ import SwiftUI
 
 struct MainScreen: View {
     @Environment(\.diManager) private var di
-    @State private var selectedTab: Tab = .flow
-    
-    @StateObject var viewModel: MainViewModel
-    
-    var body: some View {
-        TabView(selection: $selectedTab) {
-            HabitFlowScreen(viewModel: di.resolve())
-                .tabMenu(Tab.flow)
-            HabitWeekScreen(viewModel: di.resolve())
-                .tabMenu(Tab.week)
-            HabitMonthScreen(viewModel: di.resolve())
-                .tabMenu(Tab.month)
-            SettingsScreen(viewModel: di.resolve())
-                .tabMenu(Tab.settings)
-        }
-        // Hiding Tab Bar When Aheet is Expanded
-        .toolbar(.visible, for: .tabBar)
-    }
-}
+    @State private var selectedTab: AppTab = AppTab.day
 
-extension View {
-    @ViewBuilder
-    fileprivate func tabMenu(_ tab: Tab) -> some View {
-        self
-            .tag(tab)
-            .tabItem {
-                Label {
-                    Text(tab.rawValue)
-                } icon: {
-                    Image(systemName: tab.icon)
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 0) {
+                // Segmented Control
+                SegmentedView(selection: $selectedTab)
+                .padding(.horizontal)
+
+                // Content
+                TabView(selection: $selectedTab) {
+                    HabitDayScreen(viewModel: di.resolve())
+                        .tag(AppTab.day)
+//                        .transition(.slide)
+                    HabitWeekScreen(viewModel: di.resolve())
+                        .tag(AppTab.week)
+                        .transition(.slide)
+                    HabitMonthScreen(viewModel: di.resolve())
+                        .tag(AppTab.month)
+                        .transition(.slide)
+                }
+                .tabViewStyle(.page(indexDisplayMode: .never))
+                .animation(.spring(), value: selectedTab)
+            }
+            .navigationTitle("Hi, \(Profile.user?.name ?? "")")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                    } label: {
+                        Image(systemName: "plus")
+                            .font(.title2)
+                    }
                 }
             }
-            // изменить цвет TabView, необходимо для каждого, поэтому расположено тут
-            .toolbarBackground(.visible, for: .tabBar)
-            .toolbarBackground(.ultraThickMaterial, for: .tabBar)
-    }
-}
-
-private enum Tab: LocalizedStringKey, CaseIterable {
-    case flow = "Flow"
-    case week = "Week"
-    case month = "Month"
-    case settings = "Settings"
-    
-    var icon: String {
-        switch self {
-        case .flow:
-            "flag.badge.ellipsis"
-        case .week:
-            "ellipsis.bubble"
-        case .month:
-            "tablecells.badge.ellipsis"
-        case .settings:
-            "gearshape"
         }
     }
 }
 
 #Preview {
-    //    MainScreen()
+    // MainScreen(viewModel: MainViewModel(habitRepository: PreviewHabitRepository()))
 }
