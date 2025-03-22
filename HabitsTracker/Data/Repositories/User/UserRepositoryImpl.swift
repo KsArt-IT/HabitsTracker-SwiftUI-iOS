30945d4c4f53b425bdd810e021b517798f648743
KsArT-IT <ksart.it@gmail.com>
2025-03-22 18:51:18 +0200

    feat: implement UserRepositoryImpl for user creation and loading operations

---
+++ HabitsTracker/Data/Repositories/User/UserRepositoryImpl.swift
@@ -0,0 +1,36 @@
+//
+//  UserRepositoryImpl.swift
+//  HabitsTracker
+//
+//  Created by KsArT on 22.03.2025.
+//
+
+import Foundation
+
+final class UserRepositoryImpl: UserRepository {
+    private let service: DataService
+    
+    init(service: DataService) {
+        self.service = service
+    }
+    
+    func createUser(name: String) async -> Result<User, any Error> {
+        let result =  await service.createUser(name: name)
+        return switch result {
+        case .success(let user):
+                .success(user.mapToDomain())
+        case .failure(let error):
+                .failure(error)
+        }
+    }
+    
+    func loadUser(name: String) async -> Result<User, any Error> {
+        let result =  await service.loadUser(name: name)
+        return switch result {
+        case .success(let user):
+                .success(user.mapToDomain())
+        case .failure(let error):
+                .failure(error)
+        }
+    }
+}
