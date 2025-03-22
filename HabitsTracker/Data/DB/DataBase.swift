21b6b326ff487caef93abcc99744075e99fa25a4
KsArT-IT <ksart.it@gmail.com>
2025-03-22 18:49:18 +0200

    feat: add DataBase class for managing data persistence

---
+++ HabitsTracker/Data/DB/DataBase.swift
@@ -0,0 +1,30 @@
+//
+//  DataBase.swift
+//  HabitsTracker
+//
+//  Created by KsArT on 13.03.2025.
+//
+
+import SwiftData
+
+final class DataBase {
+    private var container: ModelContainer = {
+        let schema = Schema([
+            UserModel.self,
+            HabitModel.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+    
+    lazy var context: ModelContext = {
+        let context = ModelContext(self.container)
+        context.autosaveEnabled = false
+        return context
+    }()
+}
