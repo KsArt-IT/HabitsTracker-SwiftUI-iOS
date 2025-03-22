dc2985f9f35930594e42e18a5403f4b82c186df1
KsArT-IT <ksart.it@gmail.com>
2025-03-22 18:51:11 +0200

    feat: add UserRepository protocol for user creation and loading operations

---
+++ HabitsTracker/Data/Repositories/User/UserRepository.swift
@@ -0,0 +1,13 @@
+//
+//  UserRepository.swift
+//  HabitsTracker
+//
+//  Created by KsArT on 22.03.2025.
+//
+
+import Foundation
+
+protocol UserRepository: AnyObject {
+    func createUser(name: String) async -> Result<User, any Error>
+    func loadUser(name: String) async -> Result<User, any Error>
+}
