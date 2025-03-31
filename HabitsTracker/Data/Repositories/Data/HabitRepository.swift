f8ebddbcb7e4f9263fcf3241a7c7ac29b95d9a63
KsArT-IT <ksart.it@gmail.com>
2025-03-31 14:30:10 +0300

    refactor: update HabitRepository to use UUID for habit identification and include user in saveHabit method

--- HabitsTracker/Data/Repositories/Data/HabitRepository.swift
+++ HabitsTracker/Data/Repositories/Data/HabitRepository.swift
@@ -3,6 +3,6 @@ import SwiftData
 
 protocol HabitRepository: AnyObject {
     func fetchHabits() async -> Result<[Habit], any Error>
-    func fetchHabit(id: String) async -> Result<Habit, any Error>
-    func saveHabit(habit: Habit) async -> Result<Habit, any Error>
+    func fetchHabit(id: UUID) async -> Result<Habit, any Error>
+    func saveHabit(habit: Habit, user: User) async -> Result<Habit, any Error>
 }
