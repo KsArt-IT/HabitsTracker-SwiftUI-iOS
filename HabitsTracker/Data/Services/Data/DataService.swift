b79f812e26379586b4aa3ecc1a36f4fb6837eaf1
KsArT-IT <ksart.it@gmail.com>
2025-03-31 14:29:42 +0300

    refactor: update DataService to use UUID for user and habit identifiers

--- HabitsTracker/Data/Services/Data/DataService.swift
+++ HabitsTracker/Data/Services/Data/DataService.swift
@@ -9,12 +9,12 @@ import Foundation
 
 protocol DataService: AnyObject {
     func loadHabits() async -> Result<[HabitModel], any Error>
-    func loadHabit(id: String) async -> Result<HabitModel, any Error>
+    func loadHabit(id: UUID) async -> Result<HabitModel, any Error>
     
     func saveHabit(habit: HabitModel) async -> Result<HabitModel, any Error>
     
     func createUser(name: String) async -> Result<UserModel, any Error>
-    func loadUser(id: String) async -> Result<UserModel, any Error>
+    func loadUser(id: UUID) async -> Result<UserModel, any Error>
     func loadUser(name: String) async -> Result<UserModel, any Error>
-    func deleteUser(userId: String) async -> Result<Bool, any Error>
+    func deleteUser(by id: UUID) async -> Result<Bool, any Error>
 }
