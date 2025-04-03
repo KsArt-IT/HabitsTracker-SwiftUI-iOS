7cf7d630b88e309aab6aaa81eb4620db6ce0363e
KsArT-IT <ksart.it@gmail.com>
2025-04-03 23:19:22 +0300

    feat: add functionality to manage selected weekdays in HabitCreateViewModel

--- HabitsTracker/Presentation/Screens/HabitCreate/HabitCreateViewModel.swift
+++ HabitsTracker/Presentation/Screens/HabitCreate/HabitCreateViewModel.swift
@@ -11,8 +11,23 @@ final class HabitCreateViewModel: ObservableObject {
     private let repository: HabitRepository
     
     @Published var cardTitle: String = ""
+
+    @Published var periodDays: Int = 0
+    private var days: Set<WeekDays> = []
     
     init(repository: HabitRepository) {
         self.repository = repository
+    }
+    
+    public func checkDay(day: WeekDays) -> Bool {
+        days.contains(day)
+    }
+    
+    public func changeDay(day: WeekDays, checked: Bool) {
+        if checked {
+            days.insert(day)
+        } else {
+            days.remove(day)
+        }
     }
 }
