577c0cda0e6622c197759622ad23e621ad96cffd
KsArT-IT <ksart.it@gmail.com>
2025-04-03 23:16:11 +0300

    refactor: add CheckBoxView component for enhanced user interaction

---
+++ HabitsTracker/Presentation/View/CheckBoxView.swift
@@ -0,0 +1,32 @@
+//
+//  CheckBoxView.swift
+//  HabitsTracker
+//
+//  Created by KsArT on 03.04.2025.
+//
+
+import SwiftUI
+
+struct CheckBoxView: View {
+    let title: LocalizedStringKey
+    @State var checked: Bool
+    let action: (Bool) -> Void
+    
+    var body: some View {
+        HStack {
+            Image(checked ? "CheckBoxChecked" : "CheckBox")
+                .resizable()
+                .frame(width: Constants.Sizes.icon, height: Constants.Sizes.icon)
+            
+            Text(title)
+            
+            Spacer()
+        }
+        .padding(Constants.Sizes.small)
+        .contentShape(Rectangle())
+        .onTapGesture {
+            checked.toggle()
+            action(checked)
+        }
+    }
+}
