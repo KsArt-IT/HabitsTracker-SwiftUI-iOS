d1c81bf05b2c044ee1c0584cc67518bc0c2a357a
KsArT-IT <ksart.it@gmail.com>
2025-04-03 23:18:09 +0300

    feat: add ChooseWeekDaysView component for selecting weekdays

---
+++ HabitsTracker/Presentation/Screens/HabitCreate/View/ChooseWeekDaysView.swift
@@ -0,0 +1,35 @@
+//
+//  ChooseWeekDaysView.swift
+//  HabitsTracker
+//
+//  Created by KsArT on 03.04.2025.
+//
+
+import SwiftUI
+
+struct ChooseWeekDaysView: View {
+    private let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
+    let check: (WeekDays) -> Bool
+    let change: (WeekDays, Bool) -> Void
+
+    var body: some View {
+        FormTitleView("Choose a day") {
+            LazyVGrid(columns: columns, spacing: 0) {
+                ForEach(WeekDays.allCases) { day in
+                    CheckBoxView(title: day.rawValue, checked: check(day)) { checked in
+                        change(day, checked)
+                    }
+                }
+            }
+            .padding(.horizontal, Constants.Sizes.medium)
+        }
+    }
+}
+
+#Preview {
+//    ChooseDayView()
+}
