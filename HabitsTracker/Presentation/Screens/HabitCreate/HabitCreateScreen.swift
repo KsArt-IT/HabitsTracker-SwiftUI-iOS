cdb784a826a17244f867704ba9f1950206dfc0c3
KsArT-IT <ksart.it@gmail.com>
2025-04-04 23:11:09 +0300

    refactor: change HabitCreateScreen layout and add reminder functionality

--- HabitsTracker/Presentation/Screens/HabitCreate/HabitCreateScreen.swift
+++ HabitsTracker/Presentation/Screens/HabitCreate/HabitCreateScreen.swift
@@ -10,28 +10,43 @@ import SwiftUI
 struct HabitCreateScreen: View {
     @Environment(\.dismiss) private var dismiss
     @StateObject var viewModel: HabitCreateViewModel
-
+    
     var body: some View {
-        ScrollView {
-            VStack {
-                // Navigation back
-                NavTitleView {
-                    dismiss()
+        VStack {
+            // Navigation back
+            NavTitleView {
+                dismiss()
+            }
+            
+            ScrollView(showsIndicators: false) {
+                VStack {
+                    CardNameEditView(text: $viewModel.cardTitle)
+                    // How Often
+                    PeriodEditView(selected: $viewModel.periodDays) {
+                        // Your own schedule
+                        ChooseWeekDaysView(check: viewModel.checkDay, change: viewModel.changeDay)
+                    }
+                    // Choose a time
+                    ChooseTimeView(
+                        times: $viewModel.times,
+                        actionPositive: viewModel.timesAdd,
+                        actionNegative: viewModel.timesDel
+                    )
+                    // Reminder
+                    ReminderToggleView(enabled: $viewModel.reminderTimes)
                 }
-                CardNameEditView(text: $viewModel.cardTitle)
-                // How Often
-                PeriodEditView(selected: $viewModel.periodDays) {
-                    // Your own schedule
-                    ChooseWeekDaysView(check: viewModel.checkDay, change: viewModel.changeDay)
-                }
+            }
+            
+            PrimaryButton(label: "Create", disabled: viewModel.cardTitle.isEmpty) {
+                if viewModel.cardCreate() { dismiss() }
             }
         }
         .padding(.horizontal, Constants.Sizes.medium)
-        .frame(maxWidth: .infinity)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
 #Preview {
-//    HabitCreateScreen()
+    //    HabitCreateScreen()
 }
 
