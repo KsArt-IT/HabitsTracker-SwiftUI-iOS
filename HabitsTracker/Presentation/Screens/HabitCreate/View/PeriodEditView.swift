e9dbf6cdee90c54cf8b622d86193e9704714a6cc
KsArT-IT <ksart.it@gmail.com>
2025-04-04 23:08:12 +0300

    refactor: simplify PeriodEditView logic and improve animation handling

--- HabitsTracker/Presentation/Screens/HabitCreate/View/PeriodEditView.swift
+++ HabitsTracker/Presentation/Screens/HabitCreate/View/PeriodEditView.swift
@@ -8,48 +8,48 @@
 import SwiftUI
 
 struct PeriodEditView<Content: View>: View {
-    @Namespace private var animation
-    
     @Binding var selected: Int
     let content: () -> Content
     
     private var periodSelected: PeriodDays {
         if 0..<PeriodDays.allCases.count ~= selected {
-            return PeriodDays.allCases[selected]
+            PeriodDays.allCases[selected]
         } else {
-            return .daily
+            .daily
         }
     }
     
     var body: some View {
         FormTitleView("How Often") {
             ForEach(PeriodDays.allCases) { period in
-                RadioButton(title: period.rawValue, selected: periodSelected == period) {
-                    withAnimation(.spring()) {
-                        selected = PeriodDays.allCases.firstIndex(of: period) ?? 0
+                RadioButton(title: period.rawValue, selected: periodSelected == period)
+                    .onTapGesture {
+                        withAnimation {
+                            selected = PeriodDays.allCases.firstIndex(of: period) ?? 0
+                        }
                     }
-                }
             }
             .padding(.horizontal, Constants.Sizes.medium)
             
             if periodSelected == .dayOfWeek {
                 content()
-                    .transition(.opacity.combined(with: .move(edge: .top))) // Анимация появления
+                    .transition(.opacity.combined(with: .move(edge: .top)))
             }
             
             Color.clear
                 .frame(height: Constants.Sizes.medium)
         }
-        .animation(.spring(), value: selected) // Анимируем изменение `selected`
-        .matchedGeometryEffect(id: Constants.Namespace.appTabGeometryId, in: animation)
     }
 }
 
 #Preview {
-    //        PeriodEditView()
+    @Previewable @State var selected: Int = 0
+    PeriodEditView(selected: $selected) {
+        EmptyView()
+    }
 }
 
-enum PeriodDays: LocalizedStringKey, CaseIterable, Identifiable {
+fileprivate enum PeriodDays: LocalizedStringKey, CaseIterable, Identifiable {
     case daily = "Daily"
     case dayOfWeek = "Your own schedule"
     
