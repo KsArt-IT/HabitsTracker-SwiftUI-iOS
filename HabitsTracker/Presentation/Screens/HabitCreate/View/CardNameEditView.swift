ed9e2de49121051d8a305380203091a43cc2564b
KsArT-IT <ksart.it@gmail.com>
2025-04-04 23:09:09 +0300

    style: add padding to CustomTextField in CardNameEditView for improved layout

--- HabitsTracker/Presentation/Screens/HabitCreate/View/CardNameEditView.swift
+++ HabitsTracker/Presentation/Screens/HabitCreate/View/CardNameEditView.swift
@@ -13,6 +13,8 @@ struct CardNameEditView: View {
     var body: some View {
         FormTitleView("Etner Card Name") {
             CustomTextField(hint: "Name", text: $text)
+                .padding(.horizontal, Constants.Sizes.medium)
+                .padding(.bottom, Constants.Sizes.medium)
         }
     }
 }
