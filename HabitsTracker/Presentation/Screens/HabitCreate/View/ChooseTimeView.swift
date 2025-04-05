9bc830e6b6029c477acff456fbed7f08df93ba22
KsArT-IT <ksart.it@gmail.com>
2025-04-05 19:30:40 +0300

    refactor: enhance ChooseTimeView with date selection and toggle functionality

--- HabitsTracker/Presentation/Screens/HabitCreate/View/ChooseTimeView.swift
+++ HabitsTracker/Presentation/Screens/HabitCreate/View/ChooseTimeView.swift
@@ -12,15 +12,25 @@ struct ChooseTimeView: View {
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
+    @State private var date = Date()
+    @State private var dateSelectIndex = 0
+    @State private var dateSelectPresented = false
+    
     @Binding var times: [Int]
     let actionPositive: () -> Void
     let actionNegative: () -> Void
     
     var body: some View {
         FormTitleView("Choose a time") {
-            LazyVGrid(columns: columns, spacing: 0) {
+            LazyVGrid(columns: columns, spacing: Constants.Sizes.paddingVertical) {
                 ForEach(times.indices, id: \.self) { index in
-                    TimeView(time: times[index])
+                    TimeView(time: times[index]) {
+                        date = times[index].toDateFromMinutes()
+                        dateSelectIndex = index
+                        dateSelectPresented = true
+                    } toggle: {
+                        times[index] = times[index].toggleAmPm()
+                    }
                 }
             }
             .padding(.horizontal, Constants.Sizes.medium)
@@ -30,6 +40,12 @@ struct ChooseTimeView: View {
                 OutlinedButton(label: "Add Time", color: .text, disabled: times.count >= Constants.Times.maxCount, action: actionPositive)
             }
             .padding(Constants.Sizes.medium)
+        }
+        .showTimePicker($dateSelectPresented, date: $date) {
+            if 0..<times.count ~= dateSelectIndex {
+                times[dateSelectIndex] = date.toMinutesFromDate()
+                dateSelectIndex = 0
+            }
         }
     }
 }
