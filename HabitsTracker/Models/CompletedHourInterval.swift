96b4dcd33c20aa312c448fe8657775b044b8597a
KsArT-IT <ksart.it@gmail.com>
2025-04-14 20:51:14 +0300

    feat: add CompletedHourInterval model for tracking completed hour intervals

---
+++ HabitsTracker/Models/CompletedHourInterval.swift
@@ -0,0 +1,14 @@
+//
+//  CompletedHourIntervalModel.swift
+//  HabitsTracker
+//
+//  Created by KsArT on 14.04.2025.
+//
+
+import Foundation
+
+struct CompletedHourInterval: Equatable, Hashable {
+    let id: UUID
+    let time: Int // время в секундах
+    let completed: Date
+}
