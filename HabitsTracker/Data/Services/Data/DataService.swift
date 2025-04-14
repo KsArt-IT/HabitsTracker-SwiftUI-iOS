0f08d44ca4402f666ae9d801086c0dbdab391724
KsArT-IT <ksart.it@gmail.com>
2025-04-14 20:56:08 +0300

    refactor: update loadHabits method to require userId parameter

--- HabitsTracker/Data/Services/Data/DataService.swift
+++ HabitsTracker/Data/Services/Data/DataService.swift
@@ -6,15 +6,19 @@
 //
 
 import Foundation
+import Combine
 
 protocol DataService: AnyObject {
-    func loadHabits() async -> Result<[HabitModel], any Error>
+    func loadHabits(by userId: UUID) async -> Result<[HabitModel], any Error>
     func loadHabit(id: UUID) async -> Result<HabitModel, any Error>
     
     func saveHabit(habit: HabitModel) async -> Result<HabitModel, any Error>
-    
+    func deleteHabit(by id: UUID) async -> Result<Bool, any Error>
+
+    var updatePublisher: AnyPublisher<HabitModel, Never> { get }
+
     func createUser(name: String) async -> Result<UserModel, any Error>
-    func loadUser(id: UUID) async -> Result<UserModel, any Error>
-    func loadUser(name: String) async -> Result<UserModel, any Error>
+    func loadUser(id: UUID) async -> Result<UserModel?, any Error>
+    func loadUser(name: String) async -> Result<UserModel?, any Error>
     func deleteUser(by id: UUID) async -> Result<Bool, any Error>
 }
