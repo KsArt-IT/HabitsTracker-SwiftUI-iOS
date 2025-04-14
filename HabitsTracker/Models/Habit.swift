b459e05b3b3029e2f41cbd8389dc4787b521a8c3
KsArT-IT <ksart.it@gmail.com>
2025-04-14 20:53:45 +0300

    refactor: update Habit model to enforce non-optional userId and rename descript to details

--- HabitsTracker/Models/Habit.swift
+++ HabitsTracker/Models/Habit.swift
@@ -7,16 +7,22 @@
 
 import Foundation
 
-struct Habit: Identifiable {
+struct Habit: Identifiable, Equatable, Comparable, Hashable {
     let id: UUID
-    let userId: UUID?
+    let userId: UUID
 
     let title: String
-    let description: String
-    
+    let details: String
+
     let createdAt: Date
     let lastCompletedDate: Date
-    
+
     let weekDays: Set<WeekDays>
     let intervals: [HourInterval]
+    let completed: [CompletedHourInterval]
+
+    // MARK: - Comparable
+    static func < (lhs: Habit, rhs: Habit) -> Bool {
+        (lhs.title, lhs.details) < (rhs.title, rhs.details)
+    }
 }
