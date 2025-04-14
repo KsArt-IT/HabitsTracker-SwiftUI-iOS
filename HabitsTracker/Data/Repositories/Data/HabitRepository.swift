7e7e0b716393287c2c6a3fcc121d27af504c92cc
KsArT-IT <ksart.it@gmail.com>
2025-04-14 20:58:08 +0300

    refactor: update fetchHabits method to require userId parameter and clean up saveHabit method

--- HabitsTracker/Data/Repositories/Data/HabitRepository.swift
+++ HabitsTracker/Data/Repositories/Data/HabitRepository.swift
@@ -1,8 +1,11 @@
 import Foundation
 import SwiftData
+import Combine
 
 protocol HabitRepository: AnyObject {
-    func fetchHabits() async -> Result<[Habit], any Error>
+    func fetchHabits(by userId: UUID) async -> Result<[Habit], any Error>
     func fetchHabit(id: UUID) async -> Result<Habit, any Error>
-    func saveHabit(habit: Habit, user: User) async -> Result<Habit, any Error>
+    func saveHabit(habit: Habit) async -> Result<Habit, any Error>
+    
+    var updatePublisher: AnyPublisher<Habit, Never> { get }
 }
