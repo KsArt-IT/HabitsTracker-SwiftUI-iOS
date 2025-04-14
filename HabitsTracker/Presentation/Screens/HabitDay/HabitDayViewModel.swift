bd2e9a7f22aa0fc33d8888423cb0f854ce3e93fc
KsArT-IT <ksart.it@gmail.com>
2025-04-14 21:02:57 +0300

    refactor: implemented HabitDayViewModel

--- HabitsTracker/Presentation/Screens/HabitDay/HabitDayViewModel.swift
+++ HabitsTracker/Presentation/Screens/HabitDay/HabitDayViewModel.swift
@@ -6,35 +6,78 @@
 //
 
 import Foundation
+import Combine
 
 final class HabitDayViewModel: ObservableObject {
     private let habitRepository: HabitRepository
     @Published var habits: [Habit] = []
     
     private var task: Task<(), Never>?
-
+    private var cancellables: Set<AnyCancellable> = []
+    
     init(repository: HabitRepository) {
         self.habitRepository = repository
+        
+        // наблюдаем за добавлением и изменением записей
+        self.subscribeUpdate()
     }
     
-    func loadHabits(name: String) {
+    func fetchHabits() {
+        print("HabitDayViewModel: \(#function)")
         guard task == nil else { return }
-        let newTask = Task {
-            await loadHabits()
+        let newTask = Task { [weak self] in
+            await self?.fetchHabits()
+            
+            self?.task = nil
+        }
+        self.task = newTask
+    }
+    
+    private func fetchHabits() async {
+        print("HabitDayViewModel: \(#function) user=\(Profile.user?.id.uuidString ?? "")")
+        guard let user = Profile.user else { return }
+        
+        let result = await habitRepository.fetchHabits(by: user.id)
+        switch result {
+        case .success(let habits):
+            sortList(habits)
+        case .failure(let error):
+            await showMessage(error.localizedDescription)
         }
     }
     
-    private func loadHabits() async {
-        let result = await habitRepository.fetchHabits()
-    }
-    
-    func addHabit() {
-        // Будет реализовано позже
-    }
-    
     @MainActor
-    private func setHabits() {
-        
+    private func showMessage(_ message: String) {
+        print("HabitDayViewModel: message = \(message)")
+    }
+    
+    // MARK: - Update List
+    private func updateList(_ newItem: Habit) {
+        var newList = habits.filter { $0.id != newItem.id }
+        newList.append(newItem)
+        sortList(newList)
     }
 
+    private func sortList(_ newList: [Habit]) {
+        Task { [weak self] in
+            await self?.setList(newList.sorted(by: <))
+        }
+    }
+
+    @MainActor
+    private func setList(_ list: [Habit]) {
+        print("HabitDayViewModel: \(#function) count = \(list.count)")
+        guard habits != list else { return }
+        
+        habits = list
+    }
+
+    // MARK: - Subscribe
+    private func subscribeUpdate() {
+        habitRepository.updatePublisher
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .sink(receiveValue: updateList)
+            .store(in: &cancellables)
+    }
+    
 }
