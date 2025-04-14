6dac78f238f55d535e526dbca6e92ae7be218958
KsArT-IT <ksart.it@gmail.com>
2025-04-14 21:03:56 +0300

    refactor: restructure HabitDayScreen layout for better habit display and handling of empty state

--- HabitsTracker/Presentation/Screens/HabitDay/HabitDayScreen.swift
+++ HabitsTracker/Presentation/Screens/HabitDay/HabitDayScreen.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct HabitDayScreen: View {
+    private let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
     @StateObject var viewModel: HabitDayViewModel
 
     var body: some View {
-        VStack{
-            TitleTextView("Habit Day")
+        VStack(spacing: 0) {
+            if viewModel.habits.isEmpty {
+                Text("No habits")
+                    .font(Constants.Fonts.normalRegular)
+                    .foregroundStyle(Color.textGray)
+            } else {
+                ScrollView(.vertical, showsIndicators: false) {
+                    LazyVGrid(columns: columns, spacing: Constants.Sizes.spacingHabit) {
+                        ForEach(viewModel.habits) { habit in
+                            HabitItemView(habit: habit) {
+                                
+                            } edit: {
+                                
+                            }
+                        }
+                    }
+                }
+            }
         }
-        .frame(maxHeight: .infinity, alignment: .top)
-        .padding(.horizontal, Constants.Sizes.small)
+        .padding(.vertical, Constants.Sizes.medium)
+        .onAppear {
+            viewModel.fetchHabits()
+        }
     }
 }
 
