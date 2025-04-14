9ab598f16d5cbd825bed34048644275f5252f927
KsArT-IT <ksart.it@gmail.com>
2025-04-14 21:03:18 +0300

    feat: add HabitItemView component for displaying habit details

---
+++ HabitsTracker/Presentation/Screens/HabitDay/View/HabitItemView.swift
@@ -0,0 +1,119 @@
+//
+//  HabitItemView.swift
+//  HabitsTracker
+//
+//  Created by KsArT on 14.04.2025.
+//
+
+import SwiftUI
+
+struct HabitItemView: View {
+    let habit: Habit
+    let action: () -> Void
+    let edit: () -> Void
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Text(habit.title)
+                    .font(Constants.Fonts.normalRegular)
+                    .lineLimit(1)
+                    .truncationMode(.tail)
+                    .foregroundStyle(Color.text)
+                Spacer(minLength: Constants.Sizes.small)
+                Button(action: edit) {
+                    Image(systemName: "ellipsis")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(4)
+                        .frame(width: Constants.Sizes.icon, height: Constants.Sizes.icon)
+                        .padding(Constants.Sizes.habitIconPadding)
+                }
+                .foregroundStyle(Color.brandBlack)
+                .background(Color.clear)
+                .clipShape(.circle)
+            }
+            
+            Spacer()
+            
+            HStack(spacing: 0) {
+                Text("\(habit.completed.count)/\(habit.intervals.count)")
+                    .font(Constants.Fonts.accentMedium)
+                    .foregroundStyle(Color.brandText)
+                Spacer(minLength: Constants.Sizes.small)
+                Button(action: action) {
+                    Image("ArrowUpRight")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: Constants.Sizes.icon, height: Constants.Sizes.icon)
+                        .padding(Constants.Sizes.habitIconPadding)
+                }
+                .foregroundStyle(Color.brand)
+                .background(Color.brandBlack)
+                .clipShape(.circle)
+            }
+        }
+        .padding(.leading, Constants.Sizes.habitPaddingLeading)
+        .padding(.vertical, Constants.Sizes.habitPadding)
+        .padding(.trailing, Constants.Sizes.habitPadding)
+        .background(
+            RoundedRectangle(cornerRadius: Constants.Sizes.habitCornerRadius)
+                .fill(
+                    RadialGradient(
+                        colors: [.formBackground, .formBackground.opacity(0.4)],
+                        center: .topTrailing,
+                        startRadius: .zero,
+                        endRadius: 150
+                    )
+                )
+        )
+    }
+}
+
+#Preview {
+    let userId = UUID()
+    
+    HStack(spacing: 0) {
+        HabitItemView(
+            habit: Habit(
+                id: UUID(),
+                userId: userId,
+                title: "Here is a very long habit name for display testing",
+                details: "",
+                createdAt: Date.now,
+                lastCompletedDate: Date.distantFuture,
+                weekDays: [WeekDays.monday, WeekDays.tuesday],
+                intervals: [
+                    HourInterval(id: UUID(), time: 180)
+                ],
+                completed: [
+                    CompletedHourInterval(id: UUID(), time: 180, completed: Date.now)
+                ]
+            ),
+            action: {},
+            edit: {}
+        )
+        Spacer(minLength: 16)
+        HabitItemView(
+            habit: Habit(
+                id: UUID(),
+                userId: userId,
+                title: "Title Habit",
+                details: "",
+                createdAt: Date.now,
+                lastCompletedDate: Date.distantFuture,
+                weekDays: [WeekDays.monday, WeekDays.tuesday],
+                intervals: [
+                    HourInterval(id: UUID(), time: 180),
+                    HourInterval(id: UUID(), time: 200)
+                ],
+                completed: [
+                    CompletedHourInterval(id: UUID(), time: 180, completed: Date.now)
+                ]
+            ),
+            action: {},
+            edit: {}
+        )
+    }
+    .padding()
+}
