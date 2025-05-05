0860453b713e4ca613682d819f4ccbd1a4f97a6f
KsArT-IT <ksart.it@gmail.com>
2025-05-05 20:03:27 +0300

    feat: implemented DataServiceImpl

--- HabitsTracker/Data/Services/Data/DataServiceImpl.swift
+++ HabitsTracker/Data/Services/Data/DataServiceImpl.swift
@@ -24,7 +24,7 @@ final class DataServiceImpl: DataService {
     // MARK: - User
     func createUser(name: String) async -> Result<UserModel, any Error> {
         // если пользователь есть, вернем
-        if case .success(let user) = await loadUser(name: name), let user {
+        if case .success(let user) = await fetchUser(by: name), let user {
             return .success(user)
         }
         // создадим пользователя
@@ -34,24 +34,24 @@ final class DataServiceImpl: DataService {
         return .success(user)
     }
     
-    func loadUser(id: UUID) async -> Result<UserModel?, any Error> {
+    func fetchUser(by id: UUID) async -> Result<UserModel?, any Error> {
         let descriptor = FetchDescriptor<UserModel>(
             predicate: #Predicate { $0.id == id }
         )
         do {
-            let users = try loadData(descriptor)
+            let users = try fetchData(descriptor)
             return .success(users.first)
         } catch {
             return .failure(error)
         }
     }
     
-    func loadUser(name: String) async -> Result<UserModel?, any Error> {
+    func fetchUser(by name: String) async -> Result<UserModel?, any Error> {
         let descriptor = FetchDescriptor<UserModel>(
             predicate: #Predicate { $0.name == name }
         )
         do {
-            let users = try loadData(descriptor)
+            let users = try fetchData(descriptor)
             return .success(users.first)
         } catch {
             return .failure(error)
@@ -72,12 +72,17 @@ final class DataServiceImpl: DataService {
     }
     
     // MARK: - Habit
-    func loadHabits(by userId: UUID) async -> Result<[HabitModel], any Error> {
+    func fetchHabits(by userId: UUID) async -> Result<[HabitModel], any Error> {
         let descriptor = FetchDescriptor<HabitModel>(
             predicate: #Predicate { $0.userId == userId }
         )
         do {
-            let habits = try loadData(descriptor)
+            let habits = try fetchData(descriptor)
+            for (index, habit) in habits.enumerated() {
+                if case .success(let completed) = await fetchCompleteds(by: habit.id) {
+                    habits[index].completed = completed
+                }
+            }
             print("DataServiceImpl: \(#function) habits=\(habits.count)")
             return .success(habits)
         } catch {
@@ -85,12 +90,35 @@ final class DataServiceImpl: DataService {
         }
     }
     
-    func loadHabit(id: UUID) async -> Result<HabitModel, any Error> {
+    func fetchHabits(by userId: UUID, from startDate: Date, to endDate: Date) async -> Result<[HabitModel], any Error> {
+        let descriptor = FetchDescriptor<HabitModel>(
+            predicate: #Predicate {
+                $0.userId == userId && $0.createdAt <= endDate.endOfDay && $0.completedAt >= startDate.startOfDay
+            }
+        )
+        do {
+            let habits = try fetchData(descriptor)
+            for (index, habit) in habits.enumerated() {
+                if case .success(let completed) = await fetchCompleteds(by: habit.id, from: startDate, to: endDate) {
+                    habits[index].completed = completed
+                }
+            }
+            print("DataServiceImpl: \(#function) habits=\(habits.count)")
+            return .success(habits)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    func fetchHabit(by id: UUID) async -> Result<HabitModel, any Error> {
         let descriptor = FetchDescriptor<HabitModel>(
             predicate: #Predicate { $0.id == id }
         )
         do {
-            if let habit = try loadData(descriptor).first{
+            if let habit = try fetchData(descriptor).first {
+                if case .success(let completed) = await fetchCompleteds(by: habit.id) {
+                    habit.completed = completed
+                }
                 return .success(habit)
             } else {
                 return .failure(CancellationError())
@@ -100,19 +128,69 @@ final class DataServiceImpl: DataService {
         }
     }
     
-    func saveHabit(habit: HabitModel) async -> Result<HabitModel, any Error> {
+    func saveHabit(_ habit: HabitModel) async -> Result<HabitModel, any Error> {
         print("DataServiceImpl: \(#function) habit=\(habit.title)")
         modelContext.insert(habit)
         save()
-        let result = await loadHabit(id: habit.id)
-        if case .success(let data) = result {
-            updateSubject.send(data)
-        }
-        return result
+        updateSubject.send(habit)
+        return .success(habit)
     }
     
     func deleteHabit(by id: UUID) async -> Result<Bool, any Error> {
+        // удалить habit и интервалы
         let descriptor = FetchDescriptor<HabitModel>(
+            predicate: #Predicate { $0.id == id }
+        )
+        // удалить завершенные интервалы
+        let descriptorCompleted = FetchDescriptor<HourIntervalCompletedModel>(
+            predicate: #Predicate { $0.habitId == id }
+        )
+        do {
+            try await deleteData(descriptor)
+            try await deleteData(descriptorCompleted)
+            return .success(true)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    // MARK: - HourIntervalCompletedModel
+    func fetchCompleteds(by habitId: UUID) async -> Result<[HourIntervalCompletedModel], any Error> {
+        let descriptor = FetchDescriptor<HourIntervalCompletedModel>(
+            predicate: #Predicate { $0.habitId == habitId }
+        )
+        do {
+            let completeds = try fetchData(descriptor)
+            print("DataServiceImpl: \(#function) habits=\(completeds.count)")
+            return .success(completeds)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    func fetchCompleteds(by habitId: UUID, from startDate: Date, to endDate: Date) async -> Result<[HourIntervalCompletedModel], any Error> {
+        let descriptor = FetchDescriptor<HourIntervalCompletedModel>(
+            predicate: #Predicate {
+                $0.habitId == habitId && $0.completedAt >= startDate.startOfDay && $0.completedAt <= endDate.endOfDay
+            }
+        )
+        do {
+            let completed = try fetchData(descriptor)
+            print("DataServiceImpl: \(#function) intervals=\(completed.count)")
+            return .success(completed)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    func saveCompleted(_ completed: HourIntervalCompletedModel) async -> Result<HourIntervalCompletedModel, any Error> {
+        modelContext.insert(completed)
+        save()
+        return .success(completed)
+    }
+    
+    func deleteCompleted(by id: UUID) async -> Result<Bool, any Error> {
+        let descriptor = FetchDescriptor<HourIntervalCompletedModel>(
             predicate: #Predicate { $0.id == id }
         )
         do {
@@ -124,7 +202,7 @@ final class DataServiceImpl: DataService {
     }
     
     // MARK: - Load
-    private func loadData<T: PersistentModel>(_ descriptor: FetchDescriptor<T>) throws -> [T] {
+    private func fetchData<T: PersistentModel>(_ descriptor: FetchDescriptor<T>) throws -> [T] {
         try modelContext.fetch(descriptor)
     }
     
@@ -137,7 +215,7 @@ final class DataServiceImpl: DataService {
     
     @MainActor
     func deleteData<T: PersistentModel>(_ descriptor: FetchDescriptor<T>) throws {
-        let items = try loadData(descriptor)
+        let items = try fetchData(descriptor)
         guard !items.isEmpty else { return }
         for item in items {
             modelContext.delete(item)
