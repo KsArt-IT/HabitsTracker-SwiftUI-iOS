d4fdfedb901ab43139247e0aabc541db9d7755b4
KsArT-IT <ksart.it@gmail.com>
2025-05-05 20:02:15 +0300

    fix: reorder methods in HabitRepository for improved readability

--- HabitsTracker/Data/Repositories/Data/HabitRepository.swift
+++ HabitsTracker/Data/Repositories/Data/HabitRepository.swift
@@ -4,8 +4,18 @@ import Combine
 
 protocol HabitRepository: AnyObject {
     func fetchHabits(by userId: UUID) async -> Result<[Habit], any Error>
-    func fetchHabit(id: UUID) async -> Result<Habit, any Error>
-    func saveHabit(habit: Habit) async -> Result<Habit, any Error>
+    func fetchHabits(by userId: UUID, from date: Date) async -> Result<[Habit], any Error>
+    func fetchHabits(by userId: UUID, from startDate: Date, to endDate: Date) async -> Result<[Habit], any Error>
+    
+    func fetchHabit(by id: UUID) async -> Result<Habit, any Error>
+    func saveHabit(_ habit: Habit) async -> Result<Habit, any Error>
+    func deleteHabit(by id: UUID) async -> Result<Bool, any Error>
+    
+    func fetchCompleteds(by habitId: UUID) async -> Result<[HourIntervalCompleted], any Error>
+    func fetchCompleteds(by habitId: UUID, from date: Date) async -> Result<[HourIntervalCompleted], any Error>
+    func fetchCompleteds(by habitId: UUID, from startDate: Date, to endDate: Date) async -> Result<[HourIntervalCompleted], any Error>
+    func saveCompleted(by habitId: UUID, completed: HourIntervalCompleted) async -> Result<HourIntervalCompleted, any Error>
+    func deleteCompleted(by id: UUID) async -> Result<Bool, any Error>
     
     var updatePublisher: AnyPublisher<Habit, Never> { get }
 }
