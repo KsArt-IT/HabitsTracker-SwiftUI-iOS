27eec9d8e7f09f45e7f5a89c080cb30706abe745
KsArT-IT <ksart.it@gmail.com>
2025-05-07 11:28:32 +0300

    feat: add reloadHabit method to HabitRepository protocol

--- HabitsTracker/Data/Repositories/Data/HabitRepository.swift
+++ HabitsTracker/Data/Repositories/Data/HabitRepository.swift
@@ -13,9 +13,18 @@ protocol HabitRepository: AnyObject {
     
     func fetchCompleteds(by habitId: UUID) async -> Result<[HourIntervalCompleted], any Error>
     func fetchCompleteds(by habitId: UUID, from date: Date) async -> Result<[HourIntervalCompleted], any Error>
-    func fetchCompleteds(by habitId: UUID, from startDate: Date, to endDate: Date) async -> Result<[HourIntervalCompleted], any Error>
-    func saveCompleted(by habitId: UUID, completed: HourIntervalCompleted) async -> Result<HourIntervalCompleted, any Error>
+    func fetchCompleteds(
+        by habitId: UUID,
+        from startDate: Date,
+        to endDate: Date
+    ) async -> Result<[HourIntervalCompleted], any Error>
+    func saveCompleted(
+        by habitId: UUID,
+        completed: HourIntervalCompleted
+    ) async -> Result<HourIntervalCompleted, any Error>
     func deleteCompleted(by id: UUID) async -> Result<Bool, any Error>
     
+    func reloadHabit(by id: UUID)
+    var needReloadHabitPublisher: AnyPublisher<UUID, Never> { get }
     var updatePublisher: AnyPublisher<Habit, Never> { get }
 }
