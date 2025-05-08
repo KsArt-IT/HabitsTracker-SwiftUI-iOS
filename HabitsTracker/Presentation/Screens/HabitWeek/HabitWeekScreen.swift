d6a56e800cc101648f39b87838225230af99bd78
KsArT-IT <ksart.it@gmail.com>
2025-05-08 20:52:14 +0300

    feat: implemented HabitWeekScreen

--- HabitsTracker/Presentation/Screens/HabitWeek/HabitWeekScreen.swift
+++ HabitsTracker/Presentation/Screens/HabitWeek/HabitWeekScreen.swift
@@ -9,16 +9,32 @@ import SwiftUI
 
 struct HabitWeekScreen: View {
     @StateObject var viewModel: HabitWeekViewModel
-
+    @Binding var habitMenu: HabitMenu
+    
     var body: some View {
-        VStack{
-            TitleTextView("Habit Week")
+        VStack(spacing: 0) {
+            if viewModel.habitStatus.isEmpty {
+                Text("No habits")
+                    .font(Constants.Fonts.normalRegular)
+                    .foregroundStyle(Color.textGray)
+            } else {
+                ScrollView(.vertical, showsIndicators: false) {
+                    LazyVStack(spacing: Constants.Sizes.medium) {
+                        ForEach(viewModel.habitStatus) { status in
+                            HabitWeekItemView(status: status) {
+                                habitMenu = .edit(id: status.id)
+                            } delete: {
+                                viewModel.deleteHabit(by: status.id)
+                            }
+                        }
+                    }
+                }
+            }
         }
-        .frame(maxHeight: .infinity, alignment: .top)
-        .padding(.horizontal, Constants.Sizes.small)
+        .padding(.top, Constants.Sizes.medium)
     }
 }
 
 #Preview {
-//    HabitWeekScreen()
+    //    HabitWeekScreen()
 }
