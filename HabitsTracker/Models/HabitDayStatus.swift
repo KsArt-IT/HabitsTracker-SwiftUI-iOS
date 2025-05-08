4b5498a1f975ca805a45ea80f486c93765ddec8a
KsArT-IT <ksart.it@gmail.com>
2025-05-08 20:15:43 +0300

    feat: add HabitDayStatus enum to represent various states of habit completion

---
+++ HabitsTracker/Models/HabitDayStatus.swift
@@ -0,0 +1,16 @@
+//
+//  HabitDayStatus.swift
+//  HabitsTracker
+//
+//  Created by KsArT on 08.05.2025.
+//
+
+enum HabitDayStatus {
+    case completed
+    case partiallyCompleted
+    case notCompleted
+    case awaitsExecution
+    case skipped
+    case notStarted
+    case closed
+}
