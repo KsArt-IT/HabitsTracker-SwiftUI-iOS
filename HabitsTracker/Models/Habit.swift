cc73eaceeb602156800f663205dbd4fbc2dfb6c1
KsArT-IT <ksart.it@gmail.com>
2025-05-08 20:46:47 +0300

    feat: add methods to calculate weekly habit status and determine daily status

--- HabitsTracker/Models/Habit.swift
+++ HabitsTracker/Models/Habit.swift
@@ -21,14 +21,15 @@ struct Habit: Identifiable, Equatable, Comparable, Hashable {
     let weekDays: Set<WeekDays>
     let intervals: [HourInterval]
     let completed: [HourIntervalCompleted]
-
+    
     var isActive: Bool { completedAt == Date.distantFuture }
     
     // MARK: - Comparable
-    static func < (lhs: Habit, rhs: Habit) -> Bool {
+    static func < (lhs: Self, rhs: Self) -> Bool {
         (lhs.title, lhs.details) < (rhs.title, rhs.details)
     }
     
+    // MARK: - Copy
     func copyWith(
         title: String? = nil,
         details: String? = nil,
@@ -49,5 +50,69 @@ struct Habit: Identifiable, Equatable, Comparable, Hashable {
             intervals: intervals ?? self.intervals,
             completed: completed ?? self.completed,
         )
+    }
+}
+
+extension Habit {
+    func toWeekStatus(
+        current: Date,
+        start: Date,
+        end: Date,
+    ) -> HabitWeekStatus {
+        HabitWeekStatus(
+            id: self.id,
+            title: self.title,
+            habitStatus: Habit.calculateWeekStatus(
+                self,
+                currentDate: current.endOfDay,
+                start: start,
+                numberOfDays: CalendarExt.calendar.dateComponents([.day], from: start, to: end).day ?? 0
+            )
+        )
+    }
+    
+    static private func calculateWeekStatus(
+        _ habit: Habit,
+        currentDate: Date,
+        start: Date,
+        numberOfDays: Int
+    ) -> [HabitDayStatus] {
+        (0...numberOfDays).map { offset in
+            let day = CalendarExt.calendar.date(byAdding: .day, value: offset, to: start)!
+            return getStatus(habit: habit, day: day, currentDate: currentDate)
+        }
+    }
+    
+    static private func getStatus(
+        habit: Habit,
+        day: Date,
+        currentDate: Date,
+    ) -> HabitDayStatus {
+        if day > habit.completedAt {
+            return .closed
+        }
+        
+        if day < habit.createdAt {
+            return .notStarted
+        }
+        
+        if !habit.weekDays.contains(WeekDays.from(date: day)) {
+            return .skipped
+        }
+        
+        if day > currentDate {
+            return .awaitsExecution
+        }
+        
+        let completedCount = habit.completed.filter { $0.completedAt.inSameDay(day) }.count
+        
+        return switch completedCount {
+        case habit.intervals.count:
+                .completed
+        case 1...:
+                .partiallyCompleted
+        default:
+                .notCompleted
+        }
     }
 }
