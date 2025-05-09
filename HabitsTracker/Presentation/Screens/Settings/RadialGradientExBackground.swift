2561baa16c06ce58acc31143f35be884c8af5cbc
KsArT-IT <ksart.it@gmail.com>
2025-05-09 13:35:40 +0300

    feat: add RadialGradientExBackground view modifier for enhanced background styling

---
+++ HabitsTracker/Presentation/Screens/Settings/RadialGradientExBackground.swift
@@ -0,0 +1,37 @@
+//
+//  RadialGradientBackground.swift
+//  HabitsTracker
+//
+//  Created by KsArT on 09.05.2025.
+//
+
+
+import SwiftUI
+
+fileprivate struct RadialGradientExBackground: ViewModifier {
+    let endRadius: CGFloat
+    
+    func body(content: Content) -> some View {
+        content
+            .background {
+                RoundedRectangle(cornerRadius: Constants.Sizes.habitCornerRadius)
+                    .fill(
+                        RadialGradient(
+                            colors: [.formBackground, .formBackground.opacity(0.4)],
+                            center: .topTrailing,
+                            startRadius: .zero,
+                            endRadius: endRadius
+                        )
+                    )
+            }
+    }
+}
+
+// Расширение для удобного использования модификатора
+extension View {
+    func radialGradientBackground(endRadius: CGFloat) -> some View {
+        self.modifier(
+            RadialGradientExBackground(endRadius: endRadius)
+        )
+    }
+}
