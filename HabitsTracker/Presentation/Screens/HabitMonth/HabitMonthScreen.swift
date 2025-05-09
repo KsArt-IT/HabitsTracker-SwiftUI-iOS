e3575096dcc5c9024c7585d439953d7b7bc7be51
KsArT-IT <ksart.it@gmail.com>
2025-05-09 13:34:23 +0300

    feat: update HabitMonthScreen layout to handle empty habit status and improve spacing

--- HabitsTracker/Presentation/Screens/HabitMonth/HabitMonthScreen.swift
+++ HabitsTracker/Presentation/Screens/HabitMonth/HabitMonthScreen.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct HabitMonthScreen: View {
     @StateObject var viewModel: HabitMonthViewModel
-
+    @Binding var habitMenu: HabitMenu
+    
     var body: some View {
-        VStack{
-            TitleTextView("Habit Month")
+        VStack(spacing: 0) {
+            if viewModel.habitStatus.isEmpty {
+                Text("No habits")
+                    .font(Constants.Fonts.normalRegular)
+                    .foregroundStyle(Color.textGray)
+                    .frame(maxHeight: .infinity, alignment: .center)
+            } else {
+                ScrollView(.vertical, showsIndicators: false) {
+                    LazyVStack(spacing: Constants.Sizes.medium) {
+                        ForEach(viewModel.habitStatus) { status in
+                            HabitMonthItemView(status: status) {
+                                habitMenu = .edit(id: status.id)
+                            } delete: {
+                                viewModel.deleteHabit(by: status.id)
+                            }
+                        }
+                    }
+                }
+            }
         }
-        .frame(maxHeight: .infinity, alignment: .top)
-        .padding(.horizontal, Constants.Sizes.small)
+        .padding(.top, Constants.Sizes.medium)
     }
 }
 
