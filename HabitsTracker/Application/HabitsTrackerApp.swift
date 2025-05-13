1b0d90950fe8850cee5fd45442c39e85efb537a7
KsArT-IT <ksart.it@gmail.com>
2025-05-13 20:11:00 +0300

    refactor: simplify DIManager initialization in HabitsTrackerApp

--- HabitsTracker/Application/HabitsTrackerApp.swift
+++ HabitsTracker/Application/HabitsTrackerApp.swift
@@ -10,24 +10,17 @@ import SwiftUI
 @main
 struct HabitsTrackerApp: App {
     // создаем diManager и передаем его ниже
-    private var di: DIManager
+    @Environment(\.diManager) private var di
 
-    init() {
-        debugPrint(URL.applicationSupportDirectory.path(percentEncoded: false))
-        di = DIManager()
-        // notification
-        let notificationDelegate: NotificationDelegate = di.resolve(NotificationDelegate.self)!
-        // category
-        UNUserNotificationCenter.current().setNotificationCategories([notificationDelegate.getCategories()])
-        // delegate
-        UNUserNotificationCenter.current().delegate = notificationDelegate
-    }
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.diManager, di)
         }
+    }
+    
+    init() {
+        debugPrint(URL.applicationSupportDirectory.path(percentEncoded: false))
     }
     
 }
