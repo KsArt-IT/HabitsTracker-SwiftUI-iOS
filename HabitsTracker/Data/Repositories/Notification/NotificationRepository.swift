40444d24a83579e0f30b12faad04dc479c9800e3
KsArT-IT <ksart.it@gmail.com>
2025-05-16 15:21:00 +0300

    feat: Notification management added to NotificationRepository

--- HabitsTracker/Data/Repositories/Notification/NotificationRepository.swift
+++ HabitsTracker/Data/Repositories/Notification/NotificationRepository.swift
@@ -6,7 +6,15 @@
 //
 
 import Foundation
+import UserNotifications
 
 protocol NotificationRepository {
     func completed(habitId: UUID, intervalId: UUID) async -> Bool
+    
+    func setDelegate(_ delegate: any UNUserNotificationCenterDelegate)
+    func notificationStatus(_ setStatus: @escaping (Bool) -> Void)
+    func requestPermission()
+    func later(identifier: String, content: UNNotificationContent, minute: Int) async
+    func schedule(_ title: String, for notifications: [HabitNotification]) async
+    func cancel(for notifications: [HabitNotification]) async
 }
--- HabitsTracker/Data/Repositories/Notification/NotificationRepositoryImpl.swift
+++ HabitsTracker/Data/Repositories/Notification/NotificationRepositoryImpl.swift
@@ -6,26 +6,29 @@
 //
 
 import Foundation
+import UserNotifications
 
 final class NotificationRepositoryImpl: NotificationRepository {
-    private let service: DataService
+    private let dataService: DataService
+    private let notificationService: NotificationService
 
-    init(service: DataService) {
-        self.service = service
+    init(dataService: DataService, notificationService: NotificationService) {
+        self.dataService = dataService
+        self.notificationService = notificationService
     }
     
     func completed(habitId: UUID, intervalId: UUID) async -> Bool {
         print("NotificationRepositoryImpl: \(#function)")
-        if let interval = await service.fetchInterval(habitId: habitId, intervalId: intervalId) {
-            if (await service.fetchCompleted(habitId: habitId, intervalId: intervalId, date: Date.now)) == nil {
+        if let interval = await dataService.fetchInterval(habitId: habitId, intervalId: intervalId) {
+            if (await dataService.fetchCompleted(habitId: habitId, intervalId: intervalId, date: Date.now)) == nil {
                 let completed = HourIntervalCompletedModel(
                     habitId: habitId,
                     intervalId: interval.id,
                     time: interval.time,
                 )
-                if case .success(_) = await service.saveCompleted(completed) {
+                if case .success(_) = await dataService.saveCompleted(completed) {
                     // оповестим, что нужно обновить habit
-                    service.reloadHabit(by: habitId)
+                    dataService.reloadHabit(by: habitId)
                     return true
                 }
             }
@@ -33,4 +36,28 @@ final class NotificationRepositoryImpl: NotificationRepository {
         return false
     }
 
+    func setDelegate(_ delegate: any UNUserNotificationCenterDelegate) {
+        notificationService.setDelegate(delegate)
+    }
+    
+    func notificationStatus(_ setStatus: @escaping (Bool) -> Void) {
+        notificationService.notificationStatus(setStatus)
+    }
+    
+    func requestPermission() {
+        notificationService.requestPermission()
+    }
+    
+    func later(identifier: String, content: UNNotificationContent, minute: Int) async {
+        await notificationService.later(identifier: identifier, content: content, minute: minute)
+    }
+    
+    func schedule(_ title: String, for notifications: [HabitNotification]) async {
+        await notificationService.schedule(title, for: notifications)
+    }
+    
+    func cancel(for notifications: [HabitNotification]) async {
+        await notificationService.cancel(for: notifications)
+    }
+    
 }
